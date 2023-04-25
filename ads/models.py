8e9f4ac02aaf3284eebf4e83f838e69aefb37c3e
flood <teranys1986@gmail.com>
2023-04-25 14:48:04 +0300

    сделал шаг 3

--- ads/models.py
+++ ads/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Categories(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Ads(models.Model):
+    name = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    price = models.FloatField()
+    description = models.TextField()
+    address = models.CharField(max_length=200)
+    is_published = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Объявление'
+        verbose_name_plural = 'Объявления'
